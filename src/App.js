de8a5c174d5f4a07c11e122e0be3872553bc749e
shreyansh-error-89 <shreyanshrathore75649@gmail.com>
2023-04-06 23:54:21 +0530

    before buttons

--- src/App.js
+++ src/App.js
@@ -5,49 +5,50 @@ import img1 from './images/bg-1.png'
 import img2 from './images/bg-2.png'
 import img3 from './images/bg-3.png'
 import img4 from './images/bg-4.png'
-import img5 from './images/bg-5.png'
 const imagess = [
   {
     img: img1,
     heading: "Toddlers",
     colour:  "rgb(204, 204, 255)",
-    light_colour: "rgb(230, 230, 255)"
+    light_colour: "rgb(230, 230, 255)",
+    dark: "rgb(128, 128, 255)"
   },
   {
     img: img2,
     heading: "Pre Pregnancy",
     colour:  "rgba(255, 142, 142, 0.45) 24.69%",
-    light_colour: "rgba(255, 208, 216, 0.52) 100%)"
+    light_colour: "rgba(255, 208, 216, 0.52)",
+    dark: "rgb(255, 72, 160)"
+
   },
   {
     img: img3,
     heading: "Pregnancy",
     colour:  "rgb(255, 224, 204)",
-    light_colour: "rgb(255, 240, 230)"
+    light_colour: "rgb(255, 240, 230)",
+    dark: "rgb(255,72,160)"
+
   },
   {
-    img: img3,
+    img: img4,
     heading: "Infancy",
     colour:  "rgb(214, 245, 214)",
-    light_colour: "rgb(230, 255, 230)"
+    light_colour: "rgb(230, 255, 230)",
+    dark: "rgb(123,207,151)"
+
   }
 
 
 ];
 
-const images = [
-  img1,
-  img2,
-  img3,
-  img4
-]
+
 
 const App = () => {
   return (
     <div>
       <Nav/> 
-      {/* <h1>Carousel Example</h1> */}
-      <Carousel images={images} />
+      
+      <Carousel images={imagess} />
     </div>
   );
 };
--- src/Carousel.js
+++ src/Carousel.js
@@ -1,23 +1,24 @@
 import React, { useState } from 'react';
 import './Carousel.css';
+import PlayCircleFilledRoundedIcon from '@mui/icons-material/PlayCircleFilledRounded';
 
 const Carousel = ({ images }) => {
   const [currentImage, setCurrentImage] = useState(0);
 
   const handleWheel = (event) => {
     event.preventDefault();
-    
+
     // Set a speed threshold and a maximum speed to limit the sensitivity of image changes
     const speedThreshold = 100;
     const maxSpeed = 2000;
-  
+
     // Calculate the scroll speed
     const delta = Math.abs(event.deltaY);
     const speed = Math.min(delta, maxSpeed);
-  
+
     // Calculate the number of images to move based on the scroll speed
     const imagesToMove = Math.floor(speed / speedThreshold);
-  
+
     // Calculate the new image index based on scroll direction and number of images to move
     const newIndex =
       event.deltaY < 0
@@ -25,24 +26,42 @@ const Carousel = ({ images }) => {
           ? images.length - 1
           : currentImage - imagesToMove
         : currentImage === images.length - 1
-        ? 0
-        : currentImage + imagesToMove;
-  
+          ? 0
+          : currentImage + imagesToMove;
+
     // Set the new image index and wrap around if necessary
     setCurrentImage(newIndex >= images.length ? newIndex - images.length : newIndex < 0 ? images.length + newIndex : newIndex);
   };
-  
 
-  const carouselStyle = {
-    background: `url(${images[currentImage]})`,
-    transition: 'transform 0.5s ease-in-out',
+
+  const styled = {
+    background: `${images[currentImage].colour}`
   };
+
+
+  const shadow = {
+    background: `${images[currentImage].light_colour}`
+  }
   return (
-    <div className="carousel-container">
-      <div className="carousel-scroll" onWheel={handleWheel}>
-        <img src={images[currentImage]} alt="" className="carousel-image" />
+    <>
+      <div className="head_cnt">
+      <h1 className="headings">{images[currentImage].heading}</h1>
       </div>
-    </div>
+      <div className="buttons">
+        <button>Download App</button>
+        <div className="video_play" style={{backgroundColor: `${images[currentImage].light_colour}`}}>
+          <PlayCircleFilledRoundedIcon style={{color: `${images[currentImage].dark}`}}/>
+          <span>Play intro</span>
+        </div>
+      </div>
+      <div className="carousel_container">
+        <div className='shadow-circle' style={shadow}>
+          <div className="carousel-scroll" onWheel={handleWheel} style={styled}>
+            <img src={images[currentImage].img} alt="no" className="carousel-image" />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
